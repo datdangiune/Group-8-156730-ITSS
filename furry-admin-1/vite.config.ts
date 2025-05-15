e533f6fe7b8869b86b37baaf7e2af236f81738c8
Nguyen Van Thanh Dat <dat.nvt225606@sis.hust.edu.vn>
2025-05-15 11:53:57 +0700

    1

--- furry-admin-1/vite.config.ts
+++ furry-admin-1/vite.config.ts
@@ -8,7 +8,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 8082,
     // Add historyApiFallback to handle client-side routing
     historyApiFallback: true
   },
